aaa5bf3f386637a8ef5bccdbfa7f94a75982a160
tina <tinhinaneachir@gmail.com>
2022-11-17 09:47:41 +0100

    create module chat

---
+++ frontend-implicaction/src/app/chat/chat.module.ts
@@ -0,0 +1,15 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FriendsListComponent} from "./component/friends-list/friends-list.component";
+
+
+@NgModule({
+  declarations: [
+    FriendsListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ChatModule {
+}
